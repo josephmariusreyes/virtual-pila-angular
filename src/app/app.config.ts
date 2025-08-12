34988393978da9bc555fd731837012abc3d4ef2f
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2025-08-12 21:30:22 +0800

    app config updates

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,36 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, APP_INITIALIZER } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { SpeechDataService } from './services/speech-data.service';
 
 import { routes } from './app.routes';
 
+// App initializer function
+function initializeApp(speechDataService: SpeechDataService) {
+  return (): Promise<void> => {
+    return speechDataService.initLocalStorage();
+  };
+}
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    }),
+    // {
+    //   provide: APP_INITIALIZER,
+    //   useFactory: initializeApp,
+    //   deps: [SpeechDataService],
+    //   multi: true
+    // }
+  ]
 };
