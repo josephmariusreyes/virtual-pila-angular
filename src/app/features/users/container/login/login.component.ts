0b01c113e616cdcda118bd2ed8edc3f282cf51c1
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2025-08-17 08:21:02 +0800

    login component initial implementation

--- src/app/features/users/container/login/login.component.ts
+++ src/app/features/users/container/login/login.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
-  standalone:false
+  styleUrls: ['./login.component.scss'],
+  standalone: false,
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      remember: [false],
+    });
+  }
+
+  // submit handler: only show alert when form is valid
+  login(): void {
+    if (this.loginForm.valid) {
+      alert('Login clicked');
+    } else {
+      // ensure validation messages appear (no business logic added)
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+  // convenience getter for template controls
+  get loginFormControls() {
+    return this.loginForm.controls;
+  }
 }
