12694c745c8bfe247592f602d29279f21860919f
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2025-08-17 08:21:31 +0800

    initial component files

---
+++ src/app/features/customer-quelist/container/component/company-selection/company-selection/company-selection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-selection',
+  templateUrl: './company-selection.component.html',
+  styleUrl: './company-selection.component.scss',
+  standalone:false
+})
+export class CompanySelectionComponent {
+
+}
---
+++ src/app/features/customer-quelist/container/customer-quelist/customer-quelist/customer-quelist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-quelist',
+  templateUrl: './customer-quelist.component.html',
+  styleUrl: './customer-quelist.component.scss',
+  standalone:false
+})
+export class CustomerQuelistComponent {
+
+}
