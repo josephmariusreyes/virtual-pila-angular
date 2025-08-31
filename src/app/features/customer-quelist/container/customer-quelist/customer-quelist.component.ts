5bf138a96dea82c29e4b8d75aebb87549ccc25c4
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2025-08-31 15:22:51 +0800

    add company dropdown selection

--- src/app/features/customer-quelist/container/customer-quelist/customer-quelist.component.ts
+++ src/app/features/customer-quelist/container/customer-quelist/customer-quelist.component.ts
@@ -1,20 +1,46 @@
 import { Component, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+interface Company {
+  ID: string;
+  UrlAlias: string;
+  Name: string;
+  Description: string;
+  ContactNumber: string;
+  Email: string;
+  TransactionType: string[];
+}
 
 @Component({
   selector: 'app-customer-quelist',
   templateUrl: './customer-quelist.component.html',
   styleUrl: './customer-quelist.component.scss',
-  standalone:false
+  standalone: false
 })
 export class CustomerQuelistComponent {
-  
   companyName$ = signal<string | null>(null);
+  companies: Company[] = [];
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
     this.companyName$.set(this.route.snapshot.paramMap.get('companyName'));
+    this.http.get<Company[]>('/app-company.json').subscribe(data => {
+      this.companies = data;
+    });
+  }
+
+  onCompanySelect(event: Event): void {
+    const select = event.target as HTMLSelectElement;
+    const selectedAlias = select.value;
+    if (selectedAlias) {
+      this.router.navigate(['/customer-que', selectedAlias]);
+    }
   }
 
   completeService(): void {
@@ -28,5 +54,4 @@ export class CustomerQuelistComponent {
   removeCustomer(customerId: number): void {
     alert(`Remove Customer ${customerId} functionality will be implemented here`);
   }
-
 }
