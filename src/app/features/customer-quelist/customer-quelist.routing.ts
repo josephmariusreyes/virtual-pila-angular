e50065fc25feb6bdf76235a883910984d0ee6f29
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2025-09-07 19:57:28 +0800

    Routing path updates

--- src/app/features/customer-quelist/customer-quelist.routing.ts
+++ src/app/features/customer-quelist/customer-quelist.routing.ts
@@ -3,12 +3,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { CustomerQuelistComponent } from "./container/customer-quelist/customer-quelist.component";
 import { AddCustomerToQueComponent } from "./container/add-customer-to-que/add-customer-to-que.component";
-import { UsersModuleGuard } from "src/app/guard/users-module.guard";
+import { AddCustomerToQueGuard } from "src/app/guard/add-customer-to-que.guard";
 
 
 export const routes: Routes = [
-  { path: '', component: CustomerQuelistComponent },
-  { path: 'add-customer', component: AddCustomerToQueComponent, canActivate: [UsersModuleGuard] },
+  { path: '', component: CustomerQuelistComponent},
+  { path: 'add-customer', component: AddCustomerToQueComponent, canActivate: [AddCustomerToQueGuard] },
   { path: ':companyName', component: CustomerQuelistComponent }
 ];
 
